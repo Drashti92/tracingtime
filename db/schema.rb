b5fe204cd064a04cb5f22b3cd8c04817ad7539a1
drashtiblimbachiya <drashtil92@gmail.com>
2017-01-30 20:45:55 +0000

    resource generator and Post

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170129212504) do
+ActiveRecord::Schema.define(version: 20170130203042) do
+
+  create_table "posts", force: :cascade do |t|
+    t.date     "date"
+    t.text     "rationale"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
