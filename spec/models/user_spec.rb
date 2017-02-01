265555f0d1235f85bb9521739cb4ccdac80e1845
drashtiblimbachiya <drashtil92@gmail.com>
2017-02-01 21:23:34 +0000

    Implemented full name factor to user posts

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,19 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-    describe "creation" do
+    
         before do
             @user = User.create(email: "test@test.com", password: "password" , password_confirmation: "password", first_name: "Jon", last_name:"Snow")
         end
         
-        it "can be created" do
-            expect(@user).to be_valid
-        end
+        describe "creation" do
+            it "can be created" do
+                expect(@user).to be_valid
+             end
         
-        it "cannot be created without first_name, last_name" do
-          @user.first_name = nil
-          @user.last_name = nil
-          expect(@user).to_not be_valid
+            it "cannot be created without first_name, last_name" do
+                 @user.first_name = nil
+                 @user.last_name = nil
+                 expect(@user).to_not be_valid
+            end
         end
-    end
+    
+         describe "custom name methods" do
+            it 'has a full name method that combines first and last name' do
+                expect(@user.full_name).to eq("SNOW, JON")
+            end
+        end
 end
