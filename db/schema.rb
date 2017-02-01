05ee2da2e7516df174f26048da980d5ccf51ceb3
drashtiblimbachiya <drashtil92@gmail.com>
2017-02-01 08:19:49 +0000

    implemented user connection to posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170130203042) do
+ActiveRecord::Schema.define(version: 20170201075754) do
 
   create_table "posts", force: :cascade do |t|
     t.date     "date"
     t.text     "rationale"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
