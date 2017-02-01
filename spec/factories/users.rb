82fbfe11eeb0824fbb23cb7695113eb6a1c0539e
drashtiblimbachiya <drashtil92@gmail.com>
2017-02-01 23:47:44 +0000

    refactored specs usinf FactoryGirl

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,15 +1,20 @@
 FactoryGirl.define do
+  sequence :email do |n|
+    "test#{n}@example.com"
+  end
+ 
   factory :user do
     first_name 'Jon'
     last_name 'Snow'
-    email "test@test.com"
+    email { generate :email }
     password "password"
     password_confirmation "password"
   end
+ 
   factory :admin_user, class: "AdminUser" do
     first_name 'Admin'
     last_name 'User'
-    email "admin@user.com"
+    email { generate :email }
     password "password"
     password_confirmation "password"
   end
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'navigate' do
     before do
-     @user = User.create(email: "test@test.com", password: "password" , password_confirmation: "password", first_name: "Jon", last_name:"Snow")
+     @user = FactoryGirl.create(:user)
      login_as(@user, :scope => :user)
     end
     describe 'index' do
@@ -18,10 +18,10 @@ describe 'navigate' do
         end
        
         it 'has a list of posts' do
-           post1 = Post.create(date: Date.today, rationale: "Post1", user_id: @user.id)
-           post2 = Post.create(date: Date.today, rationale: "Post2", user_id: @user.id)
+           post1 = FactoryGirl.build_stubbed(:post)
+           post2 = FactoryGirl.build_stubbed(:second_post)
            visit posts_path
-           expect(page).to have_content(/Post1|Post2/)
+           expect(page).to have_content(/Rationale|content/)
         end
     end
         
