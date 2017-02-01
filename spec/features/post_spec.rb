6840ad2fa86c581b57b037a9ffa6342a21aeaac1
drashtiblimbachiya <drashtil92@gmail.com>
2017-02-01 21:00:33 +0000

    refactored index page to use Post partial

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 describe 'navigate' do
     before do
-     user = User.create(email: "test@test.com", password: "password" , password_confirmation: "password", first_name: "Jon", last_name:"Snow")
-     login_as(user, :scope => :user)
+     @user = User.create(email: "test@test.com", password: "password" , password_confirmation: "password", first_name: "Jon", last_name:"Snow")
+     login_as(@user, :scope => :user)
     end
     describe 'index' do
         before do
@@ -18,8 +18,8 @@ describe 'navigate' do
         end
        
         it 'has a list of posts' do
-           post1 = Post.create(date: Date.today, rationale: "Post1")
-           post2 = Post.create(date: Date.today, rationale: "Post2")
+           post1 = Post.create(date: Date.today, rationale: "Post1", user_id: @user.id)
+           post2 = Post.create(date: Date.today, rationale: "Post2", user_id: @user.id)
            visit posts_path
            expect(page).to have_content(/Post1|Post2/)
         end
@@ -51,4 +51,4 @@ describe 'navigate' do
                 expect(User.last.posts.last.rationale).to eq("User Association")
             end
         end
-    end
+end
