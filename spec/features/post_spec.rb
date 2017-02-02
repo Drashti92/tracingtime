2d19f188739fe5839e9e1ed7be71d417950039c6
drashtiblimbachiya <drashtil92@gmail.com>
2017-02-02 07:48:50 +0000

    Implemented New Entry Tab

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -6,26 +6,36 @@ describe 'navigate' do
      login_as(@user, :scope => :user)
     end
     describe 'index' do
-        before do
-            visit posts_path
-        end
+            before do
+                visit posts_path
+            end
     
-        it 'can be reached successfully' do
-            expect(page.status_code).to eq(200)
-        end
-        it 'has a little of Posts' do
-            expect(page).to have_content(/Posts/)
-        end
+            it 'can be reached successfully' do
+                expect(page.status_code).to eq(200)
+            end
+            it 'has a little of Posts' do
+                expect(page).to have_content(/Posts/)
+            end
        
-        it 'has a list of posts' do
-           post1 = FactoryGirl.build_stubbed(:post)
-           post2 = FactoryGirl.build_stubbed(:second_post)
-           visit posts_path
-           expect(page).to have_content(/Rationale|content/)
-        end
+            it 'has a list of posts' do
+                post1 = FactoryGirl.build_stubbed(:post)
+                post2 = FactoryGirl.build_stubbed(:second_post)
+                visit posts_path
+                expect(page).to have_content(/Rationale|content/)
+           end
+    end
+    
+    describe 'new' do
+       
+            it 'has a link from the homepage' do
+                visit root_path 
+                click_link ("new_post_from_nav")
+                expect(page.status_code).to eq(200)
+            end
+    
     end
         
-        describe 'creation' do
+    describe 'creation' do
             before do
                  visit new_post_path
              end
@@ -50,9 +60,9 @@ describe 'navigate' do
                 
                 expect(User.last.posts.last.rationale).to eq("User Association")
             end
-        end
+    end
         
-        describe 'edit' do
+     describe 'edit' do
             before do
                  @post = FactoryGirl.create(:post)
 
@@ -73,5 +83,5 @@ describe 'navigate' do
                 
                 expect(page).to have_content("Edited content")
             end
-        end
+     end
 end
