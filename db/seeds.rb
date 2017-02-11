0ad05ae97c26509033453ed07ee0b2110120f5ab
drashtiblimbachiya <drashtil92@gmail.com>
2017-02-11 03:42:01 +0000

    Implemented edit functionality for post staus

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,10 @@
 @user = User.create(email: "test@test.com", password: "password" , password_confirmation: "password", first_name: "Jon", last_name:"Snow")
 
 puts "1 User created"
+AdminUser.create(email: "admint@test.com", password: "password" , password_confirmation: "password", first_name: "Admin", last_name:"Name")
+
+
+puts "1 Admin User created"
 100.times do |post|
     Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end
