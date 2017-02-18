4cbf6eb562863d87d06e80a74454d0f98f8133ac
drashtiblimbachiya <drashtil92@gmail.com>
2017-02-18 10:15:46 +0000

    Added overtime to post model

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170210210822) do
+ActiveRecord::Schema.define(version: 20170216215214) do
 
   create_table "posts", force: :cascade do |t|
     t.date     "date"
     t.text     "rationale"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
     t.integer  "user_id"
     t.integer  "status"
+    t.decimal  "overtime_request", default: 0.0
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
