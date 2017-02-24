f490a940e12a8928f0567d3be2ce743b509ed1c9
drashtiblimbachiya <drashtil92@gmail.com>
2017-02-24 21:00:05 +0000

    Implemented pending posts on homepage admin dashboard

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -3,6 +3,7 @@ class StaticController < ApplicationController
   def homepage
   
     @pending_approvals = Post.where(status: 'submitted')
+    @recent_audit_items = AuditLog.last(10)
   
   end
 end
