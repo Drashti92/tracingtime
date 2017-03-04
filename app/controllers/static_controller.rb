4af1b3d7e8c719d3dedf6b25cb3a752532c4a620
drashtiblimbachiya <drashtil92@gmail.com>
2017-03-04 00:32:39 +0000

    added responsive buttons

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -6,7 +6,7 @@ class StaticController < ApplicationController
        @pending_approvals = Post.submitted
        @recent_audit_items = AuditLog.last(10)
      else
-        @pending_audit_confirmations = current_user.audit_logs.pending
+        @pending_audit_confirmations = current_user.audit_logs.pending.by_start_date
 
      end
   end
